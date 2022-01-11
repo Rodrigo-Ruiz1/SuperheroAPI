9595be7da2e9f7583a20fdb897d1aa71d7a80aed
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2022-01-11 04:04:50 -0500

    initial commit for homepage component

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import SuperheroCard from './SuperheroCard';
+
+const Homepage = () => {
+    //setting states
+    const [heroesList, setHeroesList] = useState([])
+    const [search, setSearch] = useState({
+        search: 1
+    });
+    const [hero, setHero] = useState([])
+
+    //fetch method for all superheroes
+    const fetchSuperheroes = async () => {
+        const response = await fetch(
+            `https://akabab.github.io/superhero-api/api/all.json`
+        ).then(response => response.json());
+        console.log(response);
+        setHeroesList(response);
+        return response;
+    };
+
+    //fetch method for superhero by id
+    const fetchSuperheroById = async (id) => {
+        console.log(id)
+        const response = await fetch(
+            `https://akabab.github.io/superhero-api/api/id/${id}.json`
+        ).then(response => response.json());
+        console.log('Response from single', response);
+        setHero(response)
+        return response;
+    }
+
+    //fetch all superheroes on page load for dropdown
+    useEffect(() => {
+        fetchSuperheroes();
+    }, [])
+
+    //method for updating state to current dropdown menu selection
+    const handleChange = (field, val) => {
+        setSearch({
+            [field]: val
+        })
+    }
+
+    //method to handle dropdown menu submit, call to single superhero fetch
+    const handleSearch = (event) => {
+        event.preventDefault();
+        console.log(search.search)
+        fetchSuperheroById(search.search)
+    }
+
+
+    return (
+        <div>
+            {/* dropdown menu */}
+            <form onSubmit={event => { handleSearch(event) }}>
+                <select onChange={(event) => handleChange('search', event.target.value)}>
+                    {heroesList.map(hero => {
+                        return (
+                            <option name={search} key={hero.id} value={hero.id}>{hero.name}</option>
+                        )
+                    })}
+                </select>
+                <input type="submit" value="Submit" />
+            </form>
+            <div id="content">
+                {/* conditional to check if hero state is not empty, passing props to render component*/}
+                {hero.length < 1  ? (
+                    <div id="welcomeBackground">
+                        <div id="welcome">
+                        <h2>Welcome to the site!</h2>
+                        <h5>Scroll through our menu to view details on your favorite superheroes</h5>
+                        </div>
+                    </div>
+                ) : (
+                    <SuperheroCard hero={hero} />
+                )}
+            </div>
+        </div>
+    )
+};
+
+export default Homepage;
